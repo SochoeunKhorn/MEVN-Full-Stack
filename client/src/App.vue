626ca5c4e6d23ad2f725f6650324d891bb89682c
Sochoeun <sochoeun1202@gmail.com>
2024-03-29 20:55:56 +0700

    create client side vue cli , router, tailwind css

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/profile">Profile</RouterLink>
+    </nav>
+  </div>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
